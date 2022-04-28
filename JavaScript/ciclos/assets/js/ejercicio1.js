2f5d0aa888de41d3dedd9cdfbe1acc272cf93bc9
y1r1tzy <yiritzygpm@gmail.com>
2022-04-28 15:53:30 -0500

    ciclos

---
+++ JavaScript/ciclos/assets/js/ejercicio1.js
@@ -0,0 +1,77 @@
+//escribe un ciclo anidado que imprimael siguiente codigo
+
+/* 
+for (let i=1; i<=5; i++){
+    for (let j=1; j<=i; j++){
+
+        document.write(`*`);
+
+
+    }
+
+document.write(`<br>`);
+
+
+}Ejercicio 1 */
+
+//Escribe un loop que haga lo siguiente:
+
+//Repetidamente imprime el valor de la variable xValue, disminuyendo por 0.5 su valor cada iteración.
+//El ciclo se mantendrá mientras que el valor de xValue sea positivo.
+
+
+/* 
+Ejercicio 2
+//Imprime todos los números impares entre 1 y 100
+
+
+Ejercicio 3
+//Escribe un ciclo while que imprima de 1 a n dentro de corchetes cuadrados
+//Por ejemplo: si n = 6 imprime [1] [2] [3] [4] [5] [6]
+
+
+
+Ejercicio 4
+//Escribe un ciclo que calcule la suma de los numeros positivos entre 1 y n
+//ejemplo: n = 5 sum = 15     
+//por que (1 + 2 + 3 + 4 +5 )
+ */
+
+
+/* let numeroPositivo = 10
+do {
+    console.log(numeroPositivo);
+    numeroPositivo=numeroPositivo-0.5;
+} while (numeroPositivo>0);
+ */
+
+/* let modulo;
+for(let i=1;i<100;i++){
+    if(i%2==0){
+        // modulo=i%2;
+        // console.log(modulo); 
+    }
+    else{
+        // modulo=i%2;
+        // console.log(modulo);
+        console.log('El numero '+i+' es impar');
+    }
+} */
+
+/* let n=6;
+let contador=1;
+while (contador<=n) {
+    document.write("["+contador+"]");
+    contador++;
+}
+ */
+
+/* let n=15;
+let suma=0;
+
+for(let i=1; i <=n; i++){
+    suma=suma+i;
+}
+console.log(suma); */
+
+
---
+++ JavaScript/ciclos/assets/js/main.js
@@ -0,0 +1,34 @@
+/* Ciclos */
+
+//for
+//for...in
+//forEach
+
+//imprimir la frase "Hola mundo" 10 veces
+//(inicializamos la variable; condicion; modificador del contador)
+/* 
+for(let i=0; i<10; i++){
+
+  console.log("Hola Mundo")
+debugger
+} */
+/* 
+//while
+let control = 1; //1
+
+while(control <= 5){ //condicion a observar
+    console.log(`Hola Mundo`, control);
+
+
+control++;//modificador de la variable de control
+
+}
+
+console.log (control); */
+
+// do...while
+let i=1
+do{
+console.log(`hola desde un do while`,i); i++;
+
+} while (i<=5);
