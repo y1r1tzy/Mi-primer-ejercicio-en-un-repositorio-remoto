552a65e1aeef810fe27ec902d7a57a3e292afdba
y1r1tzy <yiritzygpm@gmail.com>
2022-04-29 09:21:56 -0500

    guardars

---
+++ JavaScript/DOM/assets/js/main.js
@@ -0,0 +1,38 @@
+$parr.textContent += "hola mundo!";
+console.log();
+
+
+
+const $titulo =document.querySelector(`h1`);
+const $parr 
+
+const $card 
+
+
+
+
+//crear elementos de html
+
+console.log(document);
+
+
+   //appendChild, hace uso de los elemento que ya tenemos, anexar como hijo hacia otro elemento
+
+
+function crearElemento() {
+    const img = document.createElement("img");
+    img.src = "https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg"
+    // appendChild
+    $card.appendChild(img);
+  }
+  
+  crearElemento();
+
+
+
+
+
+
+
+
+
